2e22cca56fbeeb3d4c78771dc98ae250e9113407
fang duan <df1228@gmail.com>
2023-06-16 21:34:31 +0800

    feat: enable pick one available server

--- submodules/TelegramUI/Sources/AppDelegate.swift
+++ submodules/TelegramUI/Sources/AppDelegate.swift
@@ -2806,7 +2806,6 @@ private func extractAccountManagerState(records: AccountRecordsView<TelegramAcco
             // add to proxy list
             let _ = updateProxySettingsInteractively(accountManager: self.accountManager!, { settings in
                 var settings = settings
-                settings.servers.insert(proxyServerSetting, at: 0)
                 settings.servers.append(proxyServerSetting)
                 return settings
             }).start()
@@ -2819,43 +2818,43 @@ private func extractAccountManagerState(records: AccountRecordsView<TelegramAcco
             #else
             settings.enabled = true
             #endif
-            settings.activeServer = settings.servers[0]
+            // settings.activeServer = settings.servers[0]
             // settings.activeServer = settings.servers.randomElement()
-            // settings.activeServer = self.pickOneAvailableServer(proxySettings: settings)
+            settings.activeServer = self.pickOneAvailableServer(proxySettings: settings)
             return settings
         }).start()
     }
 
-    // // 从available的proxy servers里随机取一个
-    // private func pickOneAvailableServer(proxySettings: ProxySettings) -> ProxyServerSettings {
-    //     (self.context.get() |> deliverOnMainQueue).start(next: { context in
-    //         var network: Network?
-    //         if let context = context {
-    //             network = context.context.account.network // 取到network
-    //         }
+    // 从available的proxy servers里随机取一个
+    private func pickOneAvailableServer(proxySettings: ProxySettings) -> ProxyServerSettings {
+        (self.context.get() |> deliverOnMainQueue).start(next: { context in
+            var network: Network?
+            if let context = context {
+                network = context.context.account.network // 取到network
+            }
 
-    //         // 取 statusesContext 参考 ProxyListSettingsController.swift Line: 398
-    //         let statusesContext = ProxyServersStatuses(network: network, servers: proxySettings.get()
-    //             |> map { proxySettings -> [ProxyServerSettings] in
-    //                 return proxySettings.servers
-    //             })
-    //         // 取到statuses 参考 ProxyListSettingsController.swift Line: 403
-    //         let statuses = statusesContext.statuses()
+            // 取 statusesContext 参考 ProxyListSettingsController.swift Line: 398
+            let statusesContext = ProxyServersStatuses(network: network, servers: proxySettings.get()
+                |> map { proxySettings -> [ProxyServerSettings] in
+                    return proxySettings.servers
+                })
+            // 取到statuses 参考 ProxyListSettingsController.swift Line: 403
+            let statuses = statusesContext.statuses()
 
-    //         var availableServers = [ProxyServerSettings]
-    //         for server in proxySettings.servers {
-    //             // 状态 参考ProxyListSettingsController.swift Line: 248
-    //             let status: ProxyServerStatus = statuses[server]
+            var availableServers = [ProxyServerSettings]
+            for server in proxySettings.servers {
+                // 状态 参考ProxyListSettingsController.swift Line: 248
+                let status: ProxyServerStatus = statuses[server]
 
-    //             switch status {
-    //                 case let .available(Double):
-    //                     availableServers.insert(server)
-    //             }
-    //         }
-    //         let chosenOne = availableServers.randomElement()
-    //         return chosenOne
-    //     })
-    // }
+                switch status {
+                    case let .available(Double):
+                        availableServers.insert(server)
+                }
+            }
+            let chosenOne = availableServers.randomElement()
+            return chosenOne
+        })
+    }
 
 }
 
